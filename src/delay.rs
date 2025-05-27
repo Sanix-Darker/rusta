008725ee5aa9d07bf939457ec7bb59a810a3487f
sanix-darker <s4nixd@gmail.com>
2025-05-27 19:38:09 +0200

    chore: using the mod asm module instead... not quite safe tho

--- src/delay.rs
+++ src/delay.rs
@@ -1,12 +1,24 @@
-#[inline(always)]
-pub fn cycles(n: u32) {
-    unsafe {
-        core::arch::asm!(
-            "1:",
-            "subs {cnt:w}, {cnt:w}, #1", // decrement and set flags
-            "bne 1b",                    // loop while cnt != 0
-            cnt = inout(reg) n => _,     // read-only for Rust, read-write for asm
-            options(nostack)
-        );
+#[inline(never)]
+pub fn cycles(mut n: u32) {
+    while n > 0 {
+        unsafe { core::arch::asm!("nop") };
+        n -= 1;
     }
 }
+
+// TODO: fix out by using the asm
+//#[inline(always)]
+//pub fn cycles(n: u32) {
+//    // move the immutable parameter into a mutable local
+//    let mut cnt = n;
+//
+//    unsafe {
+//        core::arch::asm!(
+//            "1:",
+//            "subs {cnt}, {cnt}, #1",   // cnt--, set flags
+//            "bne 1b",                 // branch back while cnt != 0
+//            cnt = inout(reg) cnt,     // read-write GPR (Xn); no width modifier needed
+//            options(nostack)
+//        );
+//    }
+//}

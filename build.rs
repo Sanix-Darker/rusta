f23b45d990cd1ddbad10bf4344c14e39a6ca746d
sanix-darker <s4nixd@gmail.com>
2025-05-28 14:00:34 +0200

    chore: added the linked arg to the config.toml

--- build.rs
+++ build.rs
@@ -4,5 +4,4 @@ fn main() {
     println!("cargo:rerun-if-changed=memory.x");
     let out = PathBuf::from(env::var("OUT_DIR").unwrap());
     fs::copy("memory.x", out.join("memory.x")).unwrap();
-    println!("cargo:rustc-link-arg=-Tmemory.x");
 }
